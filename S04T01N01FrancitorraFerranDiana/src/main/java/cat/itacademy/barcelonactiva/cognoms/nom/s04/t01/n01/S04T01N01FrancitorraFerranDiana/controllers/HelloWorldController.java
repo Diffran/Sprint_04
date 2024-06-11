1611bcca3e1c25d66d7bd1b4f707bcc0b289828e
Diffran <148889182+Diffran@users.noreply.github.com>
2024-06-11 13:01:50 +0200

    els metodes saluda

--- S04T01N01FrancitorraFerranDiana/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01/S04T01N01FrancitorraFerranDiana/controllers/HelloWorldController.java
+++ S04T01N01FrancitorraFerranDiana/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01/S04T01N01FrancitorraFerranDiana/controllers/HelloWorldController.java
@@ -1,4 +1,19 @@
 package cat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n01.S04T01N01FrancitorraFerranDiana.controllers;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
 public class HelloWorldController {
+
+    @GetMapping("/saluda")
+    public String saluda(@RequestParam(value="nom", defaultValue="Unknown") String nom){
+        return "Hola, " + nom + ". Estàs executant un projecte Maven";
+    }
+
+    @GetMapping("/saluda2")
+    public String saluda2(@PathVariable(value="nom", required=false)String nom){
+        return "Hola, " + nom + ". Estàs executant un projecte Maven";
+    }
+
 }
