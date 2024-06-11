6a31a0f6b94721d5da3b9fdcbec30bf9b33f742d
Diffran <148889182+Diffran@users.noreply.github.com>
2024-06-11 12:33:45 +0200

    first commit 2?

---
+++ S04T01N01FrancitorraFerranDiana/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01/S04T01N01FrancitorraFerranDiana/S04T01N01FrancitorraFerranDianaApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n01.S04T01N01FrancitorraFerranDiana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01FrancitorraFerranDianaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01FrancitorraFerranDianaApplication.class, args);
+	}
+
+}
---
+++ S04T01N01FrancitorraFerranDiana/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01/S04T01N01FrancitorraFerranDiana/controllers/HelloWorldController.java
@@ -0,0 +1,4 @@
+package cat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n01.S04T01N01FrancitorraFerranDiana.controllers;
+
+public class HelloWorldController {
+}
