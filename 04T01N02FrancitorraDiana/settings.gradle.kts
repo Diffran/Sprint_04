5eae5fb2be66e216c61d1796fbdeeef20c5094ce
Diffran <148889182+Diffran@users.noreply.github.com>
2024-06-11 13:24:50 +0200

    first commit de nivel2 tasca 1

---
+++ 04T01N02FrancitorraDiana/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "04T01N02FrancitorraDiana"
---
+++ 04T01N02FrancitorraDiana/src/test/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n02/_T01N02FrancitorraDiana/ApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n02._T01N02FrancitorraDiana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
