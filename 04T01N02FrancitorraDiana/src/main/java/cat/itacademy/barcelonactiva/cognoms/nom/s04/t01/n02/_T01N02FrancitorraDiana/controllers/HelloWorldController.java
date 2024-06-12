8388ec23f5bc3805bfc881c28351ce61b0d926f7
Diffran <148889182+Diffran@users.noreply.github.com>
2024-06-12 11:10:45 +0200

    correction println -> Gradle not Maven

--- 04T01N02FrancitorraDiana/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n02/_T01N02FrancitorraDiana/controllers/HelloWorldController.java
+++ 04T01N02FrancitorraDiana/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n02/_T01N02FrancitorraDiana/controllers/HelloWorldController.java
@@ -8,11 +8,11 @@ public class HelloWorldController {
     public static final String DEFAULT_NAME="Unknown";
     @GetMapping("/saluda")
     public String saluda(@RequestParam(value="name", defaultValue=DEFAULT_NAME) String name){
-        return "Hola, " + name + ". Estàs executant un projecte Maven";
+        return "Hola, " + name + ". Estàs executant un projecte Gradle";
     }
 
     @GetMapping({"/saluda2","/saluda2/{name}"})
     public String saluda2(@PathVariable(required=false)String name){
-        return "Hola, " + name + ". Estàs executant un projecte Maven";
+        return "Hola, " + name + ". Estàs executant un projecte Gradle";
     }
 }
