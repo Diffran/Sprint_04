57c40ef0432e9d4617073fcfdcc46edafc2b4831
Diffran <148889182+Diffran@users.noreply.github.com>
2024-06-12 13:52:40 +0200

    first commit t2 n1

---
+++ S04T02N01FrancitorraDiana/src/main/java/cat/itacademy/barcelonactiva/francitorra/diana/s04/t02/n01/S04T02N01FrancitorraDiana/S04T02N01FrancitorraDianaApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.francitorra.diana.s04.t02.n01.S04T02N01FrancitorraDiana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T02N01FrancitorraDianaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T02N01FrancitorraDianaApplication.class, args);
+	}
+
+}
---
+++ S04T02N01FrancitorraDiana/src/test/java/cat/itacademy/barcelonactiva/francitorra/diana/s04/t02/n01/S04T02N01FrancitorraDiana/S04T02N01FrancitorraDianaApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.francitorra.diana.s04.t02.n01.S04T02N01FrancitorraDiana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T02N01FrancitorraDianaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
