2f8e38f72f9f308f8f29cde79eb8ad4e3245b317
Diffran <148889182+Diffran@users.noreply.github.com>
2024-06-17 11:05:36 +0200

    correccions

--- 04T01N02FrancitorraDiana/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n02/_T01N02FrancitorraDiana/controllers/HelloWorldController.java
+++ 04T01N02FrancitorraDiana/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n02/_T01N02FrancitorraDiana/controllers/HelloWorldController.java
@@ -13,6 +13,6 @@ public class HelloWorldController {
 
     @GetMapping({"/saluda2","/saluda2/{name}"})
     public String saluda2(@PathVariable(required=false)String name){
-        return "Hola, " + name + ". Estàs executant un projecte Gradle";
+        return "Hola, " + (name != null ? name:DEFAULT_NAME) + ". Estàs executant un projecte Gradle";
     }
 }
