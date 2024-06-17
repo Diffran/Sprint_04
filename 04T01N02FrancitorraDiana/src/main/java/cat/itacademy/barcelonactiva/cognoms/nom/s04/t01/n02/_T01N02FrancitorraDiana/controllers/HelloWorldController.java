c78df44ca2e5d48bb110a2b9a690a3c6118aaedb
Diffran <148889182+Diffran@users.noreply.github.com>
2024-06-17 11:37:13 +0200

    mas correcciones, nombre en ingles

--- 04T01N02FrancitorraDiana/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n02/_T01N02FrancitorraDiana/controllers/HelloWorldController.java
+++ 04T01N02FrancitorraDiana/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n02/_T01N02FrancitorraDiana/controllers/HelloWorldController.java
@@ -3,15 +3,14 @@ package cat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n02._T01N02FrancitorraD
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/")
 public class HelloWorldController {
     public static final String DEFAULT_NAME="Unknown";
-    @GetMapping("/saluda")
+    @GetMapping("/HelloWorld")
     public String saluda(@RequestParam(value="name", defaultValue=DEFAULT_NAME) String name){
         return "Hola, " + name + ". Estàs executant un projecte Gradle";
     }
 
-    @GetMapping({"/saluda2","/saluda2/{name}"})
+    @GetMapping({"/HelloWorld2","/HelloWorld2/{name}"})
     public String saluda2(@PathVariable(required=false)String name){
         return "Hola, " + (name != null ? name:DEFAULT_NAME) + ". Estàs executant un projecte Gradle";
     }
